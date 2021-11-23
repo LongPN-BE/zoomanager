c08d232e1b22e5db5c215c012fa0e1bff644fb6d
LongPN-BE <longpn.be@gmail.com>
2021-11-23 23:45:50 +0700

    first commit

---
+++ src/animalpkg/FLA.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalpkg;
+
+
+public class FLA extends Animal {
+
+    boolean canGrowl;
+    boolean danger;
+
+    public FLA(String ID, String name,AnimalType type, String color, double weight,  boolean canGrowl, boolean danger) {
+        super(ID, name, type, color, weight);
+        this.canGrowl = canGrowl;
+        this.danger = danger;
+    }
+
+    @Override
+    public String toString() {
+        return "FLA{" + super.toString() + "\n canGrowl=" + canGrowl + "\n danger=" + danger + "}\n";
+    }
+
+    @Override
+    public String strToFile() {
+        return super.strToFile() + ";" + canGrowl + ";" + danger;
+    }
+}
